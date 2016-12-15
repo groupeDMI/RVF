1b0767841c120746a1259ab897069c5ebdcca61d
rate8 <rate8@DMI.LOCAL>
2016-12-15 18:25:16 +0100

    correction encodage

--- rvf.php
+++ rvf.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Des métiers spécialisés autour du ressort</title>
-<meta http-equiv="imagetoolbar" content="no" /> 
-<meta http-equiv="MSThemeCompatible" content="no" /> 
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Des mÃ©tiers spÃ©cialisÃ©s autour du ressort</title>
+<meta http-equiv="imagetoolbar" content="no" />
+<meta http-equiv="MSThemeCompatible" content="no" />
 <meta name="MSSmartTagsPreventParsing" content="TRUE" />
-<meta name="description" content="Des métiers spécialisés autour du ressort" />
+<meta name="description" content="Des mÃ©tiers spÃ©cialisÃ©s autour du ressort" />
 <link href="ressort.css" rel="stylesheet" type="text/css" media="screen" />
 <link href="ressorts.css" rel="stylesheet" type="text/css" media="print" />
 <script language="javascript" type="text/javascript" src="scripts/swfobject.js"></script>
@@ -22,9 +22,9 @@
 	<div id="top2"> </div>
 	<ul id="nav">
 		<li id="nos-ressorts"><div>Visualisez la gamme<br />de nos ressorts<br /><span class="ressort"><a href="fabrication-ressorts.php" title="Voir toute la gamme des ressorts RVF"></a></span></div></li>
-		<li id="ressorts-industriels"><div>Fabrication de ressorts<br />pour appareillage électrique<br /><span class="ressort"><a href="ressorts-industriels.php" title="Ressorts pour appareils électroménagers et électriques"></a></span></div></li>
-		<li id="ressorts-automobile"><div>Conception de ressorts<br />pour l'industrie automobile<br /><span class="ressort"><a href="ressorts-automobile.php" title="Ressorts destinés à l'industrie automobile"></a></span></div></li>
-		<li id="ressorts-sur-mesure"><div>Création sur mesure<br />de ressorts spécifiques<br /><span class="ressort"><a href="ressorts-sur-mesure.php" title="Ressorts sur mesure"></a></span></div></li>
+		<li id="ressorts-industriels"><div>Fabrication de ressorts<br />pour appareillage Ã©lectrique<br /><span class="ressort"><a href="ressorts-industriels.php" title="Ressorts pour appareils Ã©lectromÃ©nagers et Ã©lectriques"></a></span></div></li>
+		<li id="ressorts-automobile"><div>Conception de ressorts<br />pour l'industrie automobile<br /><span class="ressort"><a href="ressorts-automobile.php" title="Ressorts destinÃ©s Ã  l'industrie automobile"></a></span></div></li>
+		<li id="ressorts-sur-mesure"><div>CrÃ©ation sur mesure<br />de ressorts spÃ©cifiques<br /><span class="ressort"><a href="ressorts-sur-mesure.php" title="Ressorts sur mesure"></a></span></div></li>
 	</ul>
 	<ul id="snav">
 		<li id="snav1"></li>
@@ -32,14 +32,14 @@
 		<li id="snav3"></li>
 		<li id="snav4"></li>
 		<li id="snav5"><a href="./" title="Accueil RVF"></a></li>
-		<li id="snav6"><a href="http://www.rvf.com/" title="Ressorts RVF" rel="sidebar" onClick="addToFavorites(this);return(false);" title="Ajoutez RVF à vos favoris"></a></li>
+		<li id="snav6"><a href="http://www.rvf.com/" title="Ressorts RVF" rel="sidebar" onClick="addToFavorites(this);return(false);" title="Ajoutez RVF Ã  vos favoris"></a></li>
 		<li id="snav7"><a href="javascript:window.print();" title="Imprimez cette page"></a></li>
 	</ul>
 	<div id="fabricant-ressorts">
 		<div id="menu">
 			<ul>
 				<li id="rvf-1"><a href="./" title="Site RVF"></a></li>
-				<li id="rvf-2b"><a href="rvf.php" title="L'expérience de RVF"></a></li>
+				<li id="rvf-2b"><a href="rvf.php" title="L'expÃ©rience de RVF"></a></li>
 				<li id="rvf-3"><a href="ressorts.php" title="Les ressorts de RVF"></a></li>
 				<li id="rvf-5"><a href="contact.php" title="Contactez RVF"></a></li>
 			</ul>
@@ -47,37 +47,37 @@
 		</div>
 		<div id="contenu">
 
-<h1>RVF : des métiers spécialisés autour du ressort</h1>
+<h1>RVF : des mÃ©tiers spÃ©cialisÃ©s autour du ressort</h1>
 <p>
-Notre métier consiste à concevoir en étroite collaboration avec les chefs de projets de nos différents clients, le ressort techniquement et économiquement idéal tout en respectant les normes internationales de qualité tant pour le produit que pour l'environnement.
+Notre mÃ©tier consiste Ã  concevoir en Ã©troite collaboration avec les chefs de projets de nos diffÃ©rents clients, le ressort techniquement et Ã©conomiquement idÃ©al tout en respectant les normes internationales de qualitÃ© tant pour le produit que pour l'environnement.
 </p>
 <p>
-RVF met au service de ses clients ses compétences dans les domaines suivants :
+RVF met au service de ses clients ses compÃ©tences dans les domaines suivants :
 <ul style="padding-left:50px;">
-<li>Aide à la <a href="ressorts-sur-mesure.php" title="Conception de ressorts sur mesure">conception des ressorts</a></li>
-<li>Proposition de conditionnements spécifiques adaptés aux besoins d'industrialisation de nos clients</li>
-<li>Intégration du ressort dans son proche environnement dans nos ateliers lorsque la fonction du ressort nécessite cette solution (qualitativement et économiquement).</li>
+<li>Aide Ã  la <a href="ressorts-sur-mesure.php" title="Conception de ressorts sur mesure">conception des ressorts</a></li>
+<li>Proposition de conditionnements spÃ©cifiques adaptÃ©s aux besoins d'industrialisation de nos clients</li>
+<li>IntÃ©gration du ressort dans son proche environnement dans nos ateliers lorsque la fonction du ressort nÃ©cessite cette solution (qualitativement et Ã©conomiquement).</li>
 </ul>
 </p>
 <p>
-Autant d'éléments pouvant contribuer à l'obtention du meilleur coût résultant de l'analyse de la valeur de la fonction ressort. 
+Autant d'Ã©lÃ©ments pouvant contribuer Ã  l'obtention du meilleur coÃ»t rÃ©sultant de l'analyse de la valeur de la fonction ressort.
 </p>
 
-<h2>Prés de 3 millions de ressorts produits par jour...</h2>
+<h2>PrÃ¨s de 3 millions de ressorts produits par jour...</h2>
 <p>
-Ressort <strong><acronym title="RVF : Ressorts et Visseries de Flandres">RVF</acronym></strong> est certifié iso 9001 version 2000 et ISO TS 16949 version 2002.
+Ressort <strong><acronym title="RVF : Ressorts et Visseries de Flandres">RVF</acronym></strong> est certifiÃ© iso 9001 version 2000 et ISO TS 16949 version 2002.
 </p>
 <p>
-Nous étudions et développons tous les moyens de contrôle, de méthode et d'essai spécifique à nos produits.
+Nous Ã©tudions et dÃ©veloppons tous les moyens de contrÃ´le, de mÃ©thode et d'essai spÃ©cifique Ã  nos produits.
 </p>
 <p>
-Les ressorts RVF participent avec ses principaux fournisseurs à l'amélioration des matières premières et des traitements de surfaces dans le but d'apporter des solutions nouvelles en permanence.
+Les ressorts RVF participent avec ses principaux fournisseurs Ã  l'amÃ©lioration des matiÃ©res premiÃ¨res et des traitements de surfaces dans le but d'apporter des solutions nouvelles en permanence.
 </p>
 <p>
-Ressort RVF produit près de 3 millions de ressorts par jour avec les exigences de nos clients du secteur de l'<a href="ressorts-automobile.php">automobile</a>, de l'<a href="ressorts-industriels.php">électroménager, de l'appareillage électrique</a>.
+Ressort RVF produit prÃ¨s de 3 millions de ressorts par jour avec les exigences de nos clients du secteur de l'<a href="ressorts-automobile.php">automobile</a>, de l'<a href="ressorts-industriels.php">Ã©lectromÃ©nager, de l'appareillage Ã©lectrique</a>.
 </p>
 <p>
-Nos ressorts sont produits et livrés quotidiennement dans le monde entier.
+Nos ressorts sont produits et livrÃ©s quotidiennement dans le monde entier.
 </p>
 
 		</div>
