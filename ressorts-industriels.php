626e14eeac74524a379fe3107b19f5a56f0e5372
rate8 <rate8@DMI.LOCAL>
2016-12-20 10:58:40 +0100

    suite correction encodage

--- ressorts-industriels.php
+++ ressorts-industriels.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>Conception de ressorts industriels :: RVF</title>
-<meta http-equiv="imagetoolbar" content="no" /> 
-<meta http-equiv="MSThemeCompatible" content="no" /> 
+<meta http-equiv="imagetoolbar" content="no" />
+<meta http-equiv="MSThemeCompatible" content="no" />
 <meta name="MSSmartTagsPreventParsing" content="TRUE" />
-<meta name="description" content="RVF conçoit des ressorts industriels" />
+<meta name="description" content="RVF conÃ§oit des ressorts industriels" />
 <link href="ressort.css" rel="stylesheet" type="text/css" media="screen" />
 <link href="ressorts.css" rel="stylesheet" type="text/css" media="print" />
 <script language="javascript" type="text/javascript" src="scripts/swfobject.js"></script>
@@ -24,9 +24,9 @@
 	<div id="top2"> </div>
 	<ul id="nav">
 		<li id="nos-ressorts"><div>Visualisez la gamme<br />de nos ressorts<br /><span class="ressort"><a href="fabrication-ressorts.php" title="Voir toute la gamme des ressorts RVF"></a></span></div></li>
-		<li id="ressort-industriel"><div><strong>Fabrication de ressorts<br />pour appareillage électrique</strong><br /></div></li>
-		<li id="ressorts-automobile"><div>Conception de ressorts<br />pour l'industrie automobile<br /><span class="ressort"><a href="ressorts-automobile.php" title="Ressorts destinés à l'industrie automobile"></a></span></div></li>
-		<li id="ressorts-sur-mesure"><div>Création sur mesure<br />de ressorts spécifiques<br /><span class="ressort"><a href="ressorts-sur-mesure.php" title="Ressorts sur mesure"></a></span></div></li>
+		<li id="ressort-industriel"><div><strong>Fabrication de ressorts<br />pour appareillage Ã©lectrique</strong><br /></div></li>
+		<li id="ressorts-automobile"><div>Conception de ressorts<br />pour l'industrie automobile<br /><span class="ressort"><a href="ressorts-automobile.php" title="Ressorts destinÃ©s Ã  l'industrie automobile"></a></span></div></li>
+		<li id="ressorts-sur-mesure"><div>CrÃ©ation sur mesure<br />de ressorts spÃ©cifiques<br /><span class="ressort"><a href="ressorts-sur-mesure.php" title="Ressorts sur mesure"></a></span></div></li>
 	</ul>
 	<ul id="snav">
 		<li id="snav1"></li>
@@ -34,14 +34,14 @@
 		<li id="snav3"></li>
 		<li id="snav4"></li>
 		<li id="snav5"><a href="./" title="Accueil RVF"></a></li>
-		<li id="snav6"><a href="http://www.rvf.com/" title="Ressorts RVF" rel="sidebar" onClick="addToFavorites(this);return(false);" title="Ajoutez RVF à vos favoris"></a></li>
+		<li id="snav6"><a href="http://www.rvf.com/" title="Ressorts RVF" rel="sidebar" onClick="addToFavorites(this);return(false);" title="Ajoutez RVF Ã  vos favoris"></a></li>
 		<li id="snav7"><a href="javascript:window.print();" title="Imprimez cette page"></a></li>
 	</ul>
 	<div id="fabricant-ressorts">
 		<div id="menu">
 			<ul>
 				<li id="rvf-1"><a href="./" title="Site RVF"></a></li>
-				<li id="rvf-2"><a href="rvf.php" title="L'expérience de RVF"></a></li>
+				<li id="rvf-2"><a href="rvf.php" title="L'expÃ©rience de RVF"></a></li>
 				<li id="rvf-3"><a href="ressorts.php" title="Les ressorts de RVF"></a></li>
 				<li id="rvf-5"><a href="contact.php" title="Contactez RVF"></a></li>
 			</ul>
@@ -52,15 +52,15 @@
 <h1>Conception de ressorts industriels</h1>
 <div class="divd"><a href="ressorts/ressorts-industriels-big.jpg"><img src="ressorts/ressorts-industriels.jpg" rel="zoom:651,433,ressort" border="0" alt="" /></a><br /><div class="legende">Echantillons de ressorts industriels<br />Zoom sur l'image</div></div>
 <p>
-En raison de la haute technicité et de l'expérience des techniciens et ingénieurs de nos bureaux d'études sur les ressorts, plusieurs grandes marques industrielles ont confié à RVF la conception de leurs ressorts depuis de nombreuses années.
+En raison de la haute technicitÃ© et de l'expÃ©rience des techniciens et ingÃ©nieurs de nos bureaux d'Ã©tudes sur les ressorts, plusieurs grandes marques industrielles ont confiÃ© Ã  RVF la conception de leurs ressorts depuis de nombreuses annÃ©es.
 </p>
 <p>
 A titre d'exemple, et sans s'y limiter, nous fournissons une grande partie de notre production de ressorts aux acteurs des secteurs industriels suivants :
 <ul class="decale">
-<li>l'industrie du bâtiment (aérations, poignées de portes et fenêtres, etc.)</li>
-<li>l'industrie de l'électroménager (aspirateurs, grille pains, machines à laver, fours, etc.)</li>
-<li>l'industrie de l'appareillage électrique (disjoncteurs, distribution de l'énergie)</li>
-<li>l'industrie cosmétique (diffuseurs de parfums)</li>
+<li>l'industrie du bÃ¢timent (aÃ©rations, poignÃ©es de portes et fenÃ©tres, etc.)</li>
+<li>l'industrie de l'Ã©lectromÃ©nager (aspirateurs, grille pains, machines Ã  laver, fours, etc.)</li>
+<li>l'industrie de l'appareillage Ã©lectrique (disjoncteurs, distribution de l'Ã©nergie)</li>
+<li>l'industrie cosmÃ©tique (diffuseurs de parfums)</li>
 <li>aussi l'industrie des jouets, les stylos, les briquets, etc.</li>
 </ul>
 </p>
--- ressorts-sur-mesure.php
+++ ressorts-sur-mesure.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>Fabrication de ressorts sur mesure :: RVF</title>
-<meta http-equiv="imagetoolbar" content="no" /> 
-<meta http-equiv="MSThemeCompatible" content="no" /> 
+<meta http-equiv="imagetoolbar" content="no" />
+<meta http-equiv="MSThemeCompatible" content="no" />
 <meta name="MSSmartTagsPreventParsing" content="TRUE" />
-<meta name="description" content="RVF produit des ressorts sur mesure suivant un cahier des charges précis" />
+<meta name="description" content="RVF produit des ressorts sur mesure suivant un cahier des charges prÃ©cis" />
 <link href="ressort.css" rel="stylesheet" type="text/css" media="screen" />
 <link href="ressorts.css" rel="stylesheet" type="text/css" media="print" />
 <script language="javascript" type="text/javascript" src="scripts/swfobject.js"></script>
@@ -24,9 +24,9 @@
 	<div id="top2"> </div>
 	<ul id="nav">
 		<li id="nos-ressorts"><div>Visualisez la gamme<br />de nos ressorts<br /><span class="ressort"><a href="fabrication-ressorts.php" title="Voir toute la gamme des ressorts RVF"></a></span></div></li>
-		<li id="ressorts-industriels"><div>Fabrication de ressorts<br />pour appareillage électrique<br /><span class="ressort"><a href="ressorts-industriels.php" title="Ressorts pour appareils électroménagers et électriques"></a></span></div></li>
-		<li id="ressorts-automobile"><div>Conception de ressorts<br />pour l'industrie automobile<br /><span class="ressort"><a href="ressorts-automobile.php" title="Ressorts destinés à l'industrie automobile"></a></span></div></li>
-		<li id="ressort-sur-mesure"><div><strong>Création sur mesure<br />de ressorts spécifiques</strong><br /></div></li>
+		<li id="ressorts-industriels"><div>Fabrication de ressorts<br />pour appareillage Ã©lectrique<br /><span class="ressort"><a href="ressorts-industriels.php" title="Ressorts pour appareils Ã©lectromÃ©nagers et Ã©lectriques"></a></span></div></li>
+		<li id="ressorts-automobile"><div>Conception de ressorts<br />pour l'industrie automobile<br /><span class="ressort"><a href="ressorts-automobile.php" title="Ressorts destinÃ©s Ã  l'industrie automobile"></a></span></div></li>
+		<li id="ressort-sur-mesure"><div><strong>CrÃ©ation sur mesure<br />de ressorts spÃ©cifiques</strong><br /></div></li>
 	</ul>
 	<ul id="snav">
 		<li id="snav1"></li>
@@ -34,14 +34,14 @@
 		<li id="snav3"></li>
 		<li id="snav4"></li>
 		<li id="snav5"><a href="./" title="Accueil RVF"></a></li>
-		<li id="snav6"><a href="http://www.rvf.com/" title="Ressorts RVF" rel="sidebar" onClick="addToFavorites(this);return(false);" title="Ajoutez RVF à vos favoris"></a></li>
+		<li id="snav6"><a href="http://www.rvf.com/" title="Ressorts RVF" rel="sidebar" onClick="addToFavorites(this);return(false);" title="Ajoutez RVF Ã  vos favoris"></a></li>
 		<li id="snav7"><a href="javascript:window.print();" title="Imprimez cette page"></a></li>
 	</ul>
 	<div id="fabricant-ressorts">
 		<div id="menu">
 			<ul>
 				<li id="rvf-1"><a href="./" title="Site RVF"></a></li>
-				<li id="rvf-2"><a href="rvf.php" title="L'expérience de RVF"></a></li>
+				<li id="rvf-2"><a href="rvf.php" title="L'expÃ©rience de RVF"></a></li>
 				<li id="rvf-3"><a href="ressorts.php" title="Les ressorts de RVF"></a></li>
 				<li id="rvf-5"><a href="contact.php" title="Contactez RVF"></a></li>
 			</ul>
@@ -52,25 +52,25 @@
 <h1>Ressorts sur mesure</h1>
 <div class="divd"><a href="ressorts/exemple-ressort-big1.jpg"><img src="ressorts/exemple-ressort-1.jpg" rel="zoom:651,433,ressort" border="0" alt="" /></a><br /><div class="legende">Exemples de ressorts &nbsp; - &nbsp; Zoom sur l'image</div></div>
 <p>
-Chaque ressort est issu d'un cahier des charges élaboré en collaboration entre nos clients et nos chefs de projets.
+Chaque ressort est issu d'un cahier des charges Ã©laborÃ© en collaboration entre nos clients et nos chefs de projets.
 </p>
 <p>
-La conception et la mise au point des ressorts est assurée par notre bureau d'étude.
+La conception et la mise au point des ressorts est assurÃ©e par notre bureau d'Ã©tude.
 </p>
 <p>
-La réalisation des prototypes se déroule chez <a href="./" title="RVF">RVF</a> afin de se rapprocher du produit "série".
+La rÃ©alisation des prototypes se dÃ©roule chez <a href="./" title="RVF">RVF</a> afin de se rapprocher du produit "sÃ©rie".
 </p>
 <p>
-Le conditionnement est défini afin d'optimiser la distribution de nos ressorts sur la chaine d'assemblage de nos clients.
+Le conditionnement est dÃ©fini afin d'optimiser la distribution de nos ressorts sur la chaine d'assemblage de nos clients.
 </p>
 <p>
-Nous avons aussi à votre disposition un département assemblage qui permet de livrer des produits semi-finis à nos clients en gérant les pièces extérieures plastiques ou autres.
+Nous avons aussi Ã  votre disposition un dÃ©partement assemblage qui permet de livrer des produits semi-finis Ã  nos clients en gÃ©rant les piÃ¨ces extÃ©rieures plastiques ou autres.
 </p>
 <p>
-Les essais d'endurance et tests divers sont réalisés dans notre laboratoire.
+Les essais d'endurance et tests divers sont rÃ©alisÃ©s dans notre laboratoire.
 </p>
 <p>
-Nous sommes en permanence à l'écoute des évolutions technologiques afin de proposer des produits innovants et faire bénéficier à nos clients de nos <strong>30 années d'expérience dans les ressorts</strong>.
+Nous sommes en permanence Ã  l'Ã©coute des Ã©volutions technologiques afin de proposer des produits innovants et faire bÃ©nÃ©ficier Ã  nos clients de nos <strong>30 annÃ©es d'expÃ©rience dans les ressorts</strong>.
 </p>
 
 		</div>
